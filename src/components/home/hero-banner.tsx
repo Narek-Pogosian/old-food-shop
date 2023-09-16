178cc0f7d1e5f391edb2f90788be4d8617ffbba9
Narek Pogosian <narekpogosian2@gmail.com>
2023-09-16 23:35:38 +0200

    fixed eslint error

--- src/components/home/hero-banner.tsx
+++ src/components/home/hero-banner.tsx
@@ -9,9 +9,9 @@ const HeroBanner = () => {
           Collection is here
         </h1>
         <p className="font-semibold text-white md:text-lg">
-          "Food, in the end, in our own tradition, is something holy. It's not
-          about nutrients and calories. It's about sharing. It's about honesty.
-          It's about identity."
+          Food, in the end, in our own tradition, is something holy. It&#39;s
+          not about nutrients and calories. It&#39;s about sharing. It&#39;s
+          about honesty. It&#39;s about identity.
           <br /> - Louise Fresco
         </p>
         <Button variant="white" asChild size="lg">
