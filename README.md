9c5d21b30b01971e9246f8b4c290ae9348be008d
Narek Pogosian <narekpogosian2@gmail.com>
2023-09-24 22:51:28 +0200

    added readme

---
+++ README.md
@@ -0,0 +1,14 @@
+# Food Shop
+
+## Tech stack
+Next.js (app router), Prisma, Tailwind CSS with shadcn/ui, Zod
+
+## View Website
+<a href="https://food-shop-roan.vercel.app/" target="_blank" >View here</a>
+
+## Includes
+1. Landing page
+2. Products list page with categort filter and sort by option
+3. Details page with read and write reviews
+4. Client side cart system thats saved in localStorage
+5. Custom checkout page with validation.
--- src/components/home/categories.tsx
+++ src/components/home/categories.tsx
@@ -7,14 +7,14 @@ const Categories = () => {
       <h2 className="mb-10 text-2xl font-semibold text-center md:text-4xl">
         Shop by category
       </h2>
-      <ul className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
+      <nav className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         {categoryData.map((category) => (
           <Link
             href={`/shop?category=${category.category}`}
             className="flex justify-center"
             key={category.category}
           >
-            <li
+            <span
               key={category.category}
               style={{ backgroundImage: `url(${category.image})` }}
               className={`sm:h-[400px] h-[350px] bg-cover bg-blend-multiply bg-gray-500 rounded p-6 w-[260px] xs:w-[300px] flex items-center justify-center 
@@ -24,10 +24,10 @@ const Categories = () => {
               <span className="text-2xl font-semibold tracking-wider text-white capitalize">
                 {category.category}
               </span>
-            </li>
+            </span>
           </Link>
         ))}
-      </ul>
+      </nav>
     </div>
   );
 };
